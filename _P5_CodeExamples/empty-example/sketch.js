d8edcb305f3c559a0acb3163dee39b0c8e964b4b
campbellaidanjr <120228131+campbellaidanjr@users.noreply.github.com>
2022-12-09 19:07:13 -0500

    Add files via upload

---
+++ _P5_CodeExamples/empty-example/sketch.js
@@ -0,0 +1,22 @@
+
+function setup() {
+  // put setup code here
+    createCanvas (500,500);
+  background (113,20,205)
+rect (200,200,100,100)    
+    
+}
+
+function draw() {
+  // put drawing code here
+  
+    if (mouseIsPressed)
+        background (100,100,100);
+    rect (200,200,100,100) 
+   
+    
+     
+}
+
+
+
